d6c230c019fa7a5f5540ab9c4fde4a1062f2a256
ikaros9052 <773875124@qq.com>
2016-10-10 01:21:19 +0800

    刘鹏

--- js/rem.js
+++ js/rem.js
@@ -2,11 +2,14 @@ window.onload=window.onresize=function(){
 	var rem=document.documentElement.clientWidth*20/320;
 	document.documentElement.style.fontSize = rem+'px';
 };
-$(".spfl").hide();
-$("#spfl").click(function(){
-	if($(".spfl").is(":visible")==true){
-		$(".spfl").slideUp("slow");
-	}else{
-		$(".spfl").slideDown("slow");
-	}
+$(function(){
+	$(".spfl").hide();
+	$("#spfl").click(function(){
+		if($(".spfl").is(":visible")==true){
+			$(".spfl").slideUp("slow");
+		}else{
+			$(".spfl").slideDown("slow");
+		}
+	})
 })
+
