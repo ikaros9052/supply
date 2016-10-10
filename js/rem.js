b0ba639424ba03a42dc859c17b55e60feb2da4fe
ikaros9052 <773875124@qq.com>
2016-10-10 09:10:36 +0800

    刘鹏

--- js/rem.js
+++ js/rem.js
@@ -2,14 +2,3 @@ window.onload=window.onresize=function(){
 	var rem=document.documentElement.clientWidth*20/320;
 	document.documentElement.style.fontSize = rem+'px';
 };
-$(function(){
-	$(".spfl").hide();
-	$("#spfl").click(function(){
-		if($(".spfl").is(":visible")==true){
-			$(".spfl").slideUp("slow");
-		}else{
-			$(".spfl").slideDown("slow");
-		}
-	})
-})
-
